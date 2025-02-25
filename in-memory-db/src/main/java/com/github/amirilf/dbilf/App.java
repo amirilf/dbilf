2aa9ae57b9b3c0865d79425fd4d29403f833a0e4
Amir Mohammad Khorasani <amirilf@protonmail.com>
2025-02-25 15:48:41 +0330

    Update cli

--- in-memory-db/src/main/java/com/github/amirilf/dbilf/App.java
+++ in-memory-db/src/main/java/com/github/amirilf/dbilf/App.java
@@ -1,33 +1,11 @@
 package com.github.amirilf.dbilf;
 
-import java.util.Scanner;
-import com.github.amirilf.dbilf.cli.DatabaseServer;
-import com.github.amirilf.dbilf.query.QueryEngine;
+import com.github.amirilf.dbilf.cli.ServerHandler;
 
 public class App {
     public static void main(String[] args) {
-        // havin multiple sessions (each session is a new thread)
-        if (args.length > 0 && args[0].equalsIgnoreCase("server")) {
-            int port = 9090;
-            if (args.length > 1) {
-                port = Integer.parseInt(args[1]);
-            }
-            DatabaseServer server = new DatabaseServer(port);
-            server.start();
-        } else {
-            // local
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("<<DBILF>>\n'exit' to disconnect.");
-            while (true) {
-                System.out.print("> ");
-                String command = scanner.nextLine();
-                if (command.equalsIgnoreCase("exit")) {
-                    break;
-                }
-                String result = QueryEngine.execute(command);
-                System.out.println(result);
-            }
-            scanner.close();
-        }
+        int port = 9090;
+        ServerHandler server = new ServerHandler(port);
+        server.start();
     }
 }
